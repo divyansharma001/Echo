6e97bbf8b211140a26c14a2d6ceeb0370b3a5fcb
Turbobot <turbobot@vercel.com>
2025-01-12 22:56:10 +0530

    feat(create-turbo): apply official-starter transform

--- geekroom-auth/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="items-center justify-items-center min-h-screen font-[family-name:var(--font-geist-sans)]">
-     Hello world
-    </div>
-  );
-}
