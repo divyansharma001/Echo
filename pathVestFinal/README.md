16fa8b4ca1a7c85b8718fe0b135fda4dc21fc376
Divyansh Sharma <140371139+divyansharma001@users.noreply.github.com>
2025-03-22 10:13:41 +0530

    fixed some changes

--- pathVestFinal/README.md
+++
@@ -1,1 +0,0 @@
-Pathvest is an AI powered personalised financial advisor. Made using next.js.
--- pathVestFinal/prisma/migrations/20241020093950_fixed/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ALTER COLUMN "lastName" DROP NOT NULL,
-ALTER COLUMN "password" DROP NOT NULL;
